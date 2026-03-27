69bf5bad72acf8e2835a87c9e45c81b1bbdf2de0
Pruth <pruth@bindu.dev>
2026-03-28 00:51:48 +0530

    fix: Set logout redirection to Home Page for all user roles

--- src/components/employer-sidebar.tsx
+++ src/components/employer-sidebar.tsx
@@ -86,7 +86,7 @@ export function EmployerSidebar() {
       </div>
       <div className="px-3 py-2">
         <button
-          onClick={() => signOut({ callbackUrl: "/login" })}
+          onClick={() => signOut({ callbackUrl: "/" })}
           className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition text-zinc-400"
         >
           <div className="flex items-center flex-1">
